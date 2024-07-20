9d1a3bda1a5ce78eea7ad839c439e094af91ed80
codethi <docencia.thiago@gmail.com>
2024-07-20 20:10:34 -0300

    feat: create and find users

---
+++ src/modules/users/user.controllers.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.services';
+
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  async list() {
+    return await this.userService.list();
+  }
+
+  @Get(':id')
+  async show(@Param('id') id: string) {
+    return await this.userService.show(id);
+  }
+
+  @Post()
+  async createUser(@Body() body: any) {
+    return await this.userService.createUser(body);
+  }
+}
---
+++ src/modules/users/user.services.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  createUser(body: any): Promise<User> {
+    return this.prisma.user.create({ data: body });
+  }
+
+  list() {
+    return this.prisma.user.findMany();
+  }
+
+  show(id: string) {
+    return this.prisma.user.findUnique({ where: { id: Number(id) } });
+  }
+}
