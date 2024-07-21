9444ce3d85a50903e63c70bd8475d6cc9bf682f3
codethi <docencia.thiago@gmail.com>
2024-07-21 09:54:25 -0300

    feat: exclud fields in return of users

---
+++ src/modules/prisma/utils/userSelectFields.ts
@@ -0,0 +1,9 @@
+export const userSectectFields = {
+  id: true,
+  email: true,
+  name: true,
+  password: false,
+  role: true,
+  createdAt: true,
+  updatedAt: false,
+};
--- src/modules/users/user.services.ts
+++ src/modules/users/user.services.ts
@@ -4,6 +4,7 @@ import { User } from '@prisma/client';
 import { CreateUserDTO } from './domain/dto/createUser.dto';
 import { UpdateUserDTO } from './domain/dto/updateUser.dto';
 import * as bcrypt from 'bcrypt';
+import { userSectectFields } from '../prisma/utils/userSelectFields';
 
 @Injectable()
 export class UserService {
@@ -11,11 +12,16 @@ export class UserService {
 
   async create(body: CreateUserDTO): Promise<User> {
     body.password = await this.hashPassword(body.password);
-    return await this.prisma.user.create({ data: body });
+    return await this.prisma.user.create({
+      data: body,
+      select: userSectectFields,
+    });
   }
 
   async list() {
-    return await this.prisma.user.findMany();
+    return await this.prisma.user.findMany({
+      select: userSectectFields,
+    });
   }
 
   async show(id: number) {
@@ -33,6 +39,7 @@ export class UserService {
     return await this.prisma.user.update({
       where: { id },
       data: body,
+      select: userSectectFields,
     });
   }
 
@@ -44,6 +51,7 @@ export class UserService {
   private async isIdExists(id: number) {
     const user = await this.prisma.user.findUnique({
       where: { id },
+      select: userSectectFields,
     });
 
     if (!user) {
