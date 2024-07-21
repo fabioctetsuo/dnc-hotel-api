9f3a6f358ad4b947b3a8bdbe24af160552383043
codethi <docencia.thiago@gmail.com>
2024-07-21 19:19:02 -0300

    feat: send email

--- src/modules/reservations/services/createReservations.service.ts
+++ src/modules/reservations/services/createReservations.service.ts
@@ -11,6 +11,8 @@ import { parseISO, differenceInDays } from 'date-fns';
 import { IHotelRepository } from 'src/modules/hotels/domain/repositories/Ihotel.repositories';
 import { ReservationStatus } from '@prisma/client';
 import { REPOSITORY_TOKEN_HOTEL } from 'src/modules/hotels/utils/repositoriesTokens';
+import { MailerService } from '@nestjs-modules/mailer';
+import { UserService } from 'src/modules/users/user.services';
 @Injectable()
 export class CreateReservationsService {
   constructor(
@@ -18,6 +20,8 @@ export class CreateReservationsService {
     private readonly reservationRepository: IReservationRepository,
     @Inject(REPOSITORY_TOKEN_HOTEL)
     private readonly hotelRepository: IHotelRepository,
+    private readonly mailerService: MailerService,
+    private readonly userService: UserService,
   ) {}
 
   async create(id: number, data: CreateReservationDto) {
@@ -51,6 +55,22 @@ export class CreateReservationsService {
       userId: id,
       status: ReservationStatus.PENDING,
     };
+
+    const hotelOwner = await this.userService.show(hotel.ownerId);
+
+    await this.mailerService.sendMail({
+      to: hotelOwner.email,
+      subject: 'Pending Reservation Approval',
+      html: `
+        <div style="font-family: Arial, sans-serif; padding: 20px; color: #333; text-align: center; border: 2px solid #041d40; border-radius: 10px; margin: auto; width: 60%;">
+            <h1 style="color: #041d40;">Pending Reservation Approval</h1>
+            <h3 style="color: #041d40;">Dear Hotel Owner,</h3>
+            <p style="font-size: 16px; color: #333;">You have a new reservation pending approval. Please review the reservation details and approve or decline the reservation at your earliest convenience.</p>
+            <p style="font-size: 16px; color: #333;">To view the reservation, please access your hotel owner profile
+            <p style="margin-top: 20px;">Thank you for your prompt attention to this matter.<br>Best regards,<br><span style="font-weight: bold; color: #041d40;">DNC Hotel Management System</span></p>
+        </div>
+      `,
+    });
 
     return this.reservationRepository.create(newReservation);
   }
