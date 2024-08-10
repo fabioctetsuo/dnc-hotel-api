5ec73cb08d7c1cc4a7635f23826df5c1d0e0aa39
Fabio Tetsuo <fabioctetsuo@gmail.com>
2024-08-10 02:04:39 -0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const allowedOrigins = [
+  'http://localhost:3001',
+  'https://nest-away-ui.vercel.app/',
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,PATCH,POST,DELETE',
   });
   //app.useGlobalInterceptors(new LoggingInterceptor());
